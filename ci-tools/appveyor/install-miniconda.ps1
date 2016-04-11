e6550554cf43e61ce1b6b4f21d9ca5ada7e1a150
Leonardo Uieda <leouieda@gmail.com>
2016-04-11 19:55:50 -0300

    Try to fix matrix options
    Simplify the conda install script

--- ci-tools/appveyor/install-miniconda.ps1
+++ ci-tools/appveyor/install-miniconda.ps1
@@ -6,10 +6,6 @@
 
 $MINICONDA_URL = "http://repo.continuum.io/miniconda/"
 
-if (! $env:ASTROPY_LTS_VERSION) {
-   $env:ASTROPY_LTS_VERSION = "1.0"
-}
-
 function DownloadMiniconda ($version, $platform_suffix) {
     $webclient = New-Object System.Net.WebClient
     $filename = "Miniconda-" + $version + "-Windows-" + $platform_suffix + ".exe"
@@ -78,14 +74,14 @@ $env:PATH = "${env:PYTHON};${env:PYTHON}\Scripts;" + $env:PATH
 # Conda config
 conda config --set always_yes true
 
-if (! $env:CONDA_CHANNELS) {
-   $CONDA_CHANNELS=@("astropy", "astropy-ci-extras", "openastronomy")
-} else {
-   $CONDA_CHANNELS=$env:CONDA_CHANNELS.split(" ")
-}
-foreach ($CONDA_CHANNEL in $CONDA_CHANNELS) {
-   conda config --add channels $CONDA_CHANNEL
-}
+#if (! $env:CONDA_CHANNELS) {
+   #$CONDA_CHANNELS=@("astropy", "astropy-ci-extras", "openastronomy")
+#} else {
+   #$CONDA_CHANNELS=$env:CONDA_CHANNELS.split(" ")
+#}
+#foreach ($CONDA_CHANNEL in $CONDA_CHANNELS) {
+   #conda config --add channels $CONDA_CHANNEL
+#}
 
 # Install the build and runtime dependencies of the project.
 conda update -q conda
@@ -112,62 +108,3 @@ if ($env:NUMPY_VERSION) {
 } else {
     $NUMPY_OPTION = ""
 }
-
-# Check whether a specific version of Astropy is required
-if ($env:ASTROPY_VERSION) {
-    if($env:ASTROPY_VERSION -match "stable") {
-        $ASTROPY_OPTION = "astropy"
-    } elseif($env:ASTROPY_VERSION -match "dev") {
-        $ASTROPY_OPTION = "Cython pip jinja2".Split(" ")
-    } elseif($env:ASTROPY_VERSION -match "lts") {
-        $ASTROPY_OPTION = "astropy=" + $env:ASTROPY_LTS_VERSION
-    } else {
-        $ASTROPY_OPTION = "astropy=" + $env:ASTROPY_VERSION
-    }
-} else {
-    $ASTROPY_OPTION = ""
-}
-
-# Install the specified versions of numpy and other dependencies
-if ($env:CONDA_DEPENDENCIES) {
-    $CONDA_DEPENDENCIES = $env:CONDA_DEPENDENCIES.split(" ")
-} else {
-    $CONDA_DEPENDENCIES = ""
-}
-
-conda install -n test -q pytest $NUMPY_OPTION $ASTROPY_OPTION $CONDA_DEPENDENCIES
-
-# Check whether the developer version of Numpy is required and if yes install it
-if ($env:NUMPY_VERSION -match "dev") {
-   Invoke-Expression "${env:CMD_IN_ENV} pip install git+http://github.com/numpy/numpy.git#egg=numpy --upgrade --no-deps"
-}
-
-# Check whether the developer version of Astropy is required and if yes install
-# it. We need to include --no-deps to make sure that Numpy doesn't get upgraded.
-if ($env:ASTROPY_VERSION -match "dev") {
-   Invoke-Expression "${env:CMD_IN_ENV} pip install git+http://github.com/astropy/astropy.git#egg=astropy --upgrade --no-deps"
-}
-
-# We finally install the dependencies listed in PIP_DEPENDENCIES. We do this
-# after installing the Numpy versions of Numpy or Astropy. If we didn't do this,
-# then calling pip earlier could result in the stable version of astropy getting
-# installed, and then overritten later by the dev version (which would waste
-# build time)
-
-if ($env:PIP_FLAGS) {
-    $PIP_FLAGS = $env:PIP_FLAGS
-} else {
-    $PIP_FLAGS = ""
-}
-
-if ($env:PIP_DEPENDENCIES) {
-    $PIP_DEPENDENCIES = $env:PIP_DEPENDENCIES
-} else {
-    $PIP_DEPENDENCIES = ""
-}
-
-if ($env:PIP_DEPENDENCIES) {
-    pip install $PIP_DEPENDENCIES $PIP_FLAGS
-}
-
-
