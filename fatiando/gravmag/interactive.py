574be9ef2a054915b9dea9d1081579f71186d2fd
Santiago Soler <santiago.r.soler@gmail.com>
2017-11-16 18:00:22 -0300

    Added force_CW in Moulder to avoid wrong computation.

--- fatiando/gravmag/interactive.py
+++ fatiando/gravmag/interactive.py
@@ -207,7 +207,7 @@ class Moulder(object):
         """
         The polygon model drawn as :class:`fatiando.mesher.Polygon` objects.
         """
-        m = [Polygon(p.xy, {'density': d})
+        m = [Polygon(p.xy, {'density': d}, force_CW=True)
              for p, d in zip(self.polygons, self.densities)]
         return m
 
