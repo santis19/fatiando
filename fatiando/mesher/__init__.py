f3cea180af51a3025d91d78605f94b828b0fc9bf
Santiago Soler <santiago.r.soler@gmail.com>
2017-11-28 14:46:33 -0300

    Added PolygonVertical and PolygonHorizontal in mesher/__init__.py

--- fatiando/mesher/__init__.py
+++ fatiando/mesher/__init__.py
@@ -3,6 +3,6 @@ Generate and operate on geometric elements and meshes.
 """
 from __future__ import absolute_import
 
-from .geometry import Polygon, Square, Prism, Tesseroid, Sphere
-from .geometry import PolygonalPrism
+from .geometry import PolygonHorizontal, PolygonVertical, Square, Prism
+from .geometry import PolygonalPrism, Tesseroid, Sphere
 from .mesh import SquareMesh, PointGrid, PrismRelief, PrismMesh, TesseroidMesh
